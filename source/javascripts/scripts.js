fd350b6aa06d39eaa14bb83eb8a941f039fdb17e
Michael Fennen <fennen529@gmail.com>
2014-12-07 10:58:56 -0500

    new commit not ready for qa

---
+++ source/javascripts/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+ $('.bxslider').bxSlider({
+  auto: true
+ });
+
+ $('.wrapper-dropdown').on('click', function() {
+    $(this).toggleClass('active');
+ });
+
+  // var maxHeight = Math.max($(".description").height(), $(".product-shots").height());
+  // var columnTop = $(".fixed").parent().offset().top;
+
+  // $(".description").height(maxHeight);
+  // $(".product-shots").height(maxHeight);
+
+  // $(window).on('scroll', function() {
+  //   var windowTop = $(window).scrollTop();
+
+  //   if (windowTop >= columnTop) {
+  //     $(".fixed").css("top", windowTop - columnTop);
+  //   } else {
+  //     $(".fixed").removeAttr("style");
+  //   }
+  // });
+
+
+ 
+
+});
+
