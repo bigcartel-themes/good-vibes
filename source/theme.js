7563adcb7fc8397164e9b3c8a6563aec840faeb8
Nick Endle <nick@bigcartel.com>
2024-02-27 20:36:29 -0600

    Add new library and settings for product page images

--- source/theme.js
+++ source/theme.js
@@ -1,5 +1,8 @@
 //= require_directory ./javascripts/vendor
+//= require javascripts/functions
 //= require javascripts/store
 //= require javascripts/cart
 //= require javascripts/dropdowns.js
 //= require javascripts/product-option-groups
+//= require javascripts/product
+//= require javascripts/product-carousel
